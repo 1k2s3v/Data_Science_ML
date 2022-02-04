adf6910226ba04bf42f98b6d131524bb0abd5def
1k2s3v <svyatoslavkalina@gmail.com>
2022-02-05 01:56:54 +0200

    Finish

---
+++ Model_deployment/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+# Create Flask App
+app = Flask(__name__)
+
+
+# Create API routing call
+@app.route('/predict', methods=['POST'])
+def predict():
+    
+    # Get JSON Request
+    feat_data = request.json
+    # Convert JSON request to Pandas DataFrame
+    df = pd.DataFrame(feat_data)
+    # Match Column Na,es
+    df = df.reindex(columns=col_names)
+    # Get prediction
+    prediction = list(model.predict(df))
+    # Return JSON version of Prediction
+    return jsonify({'prediction': str(prediction)})
+
+        
+
+if __name__ == '__main__':
+
+    # LOADS MODEL AND FEATURE COLUMNS
+    model = joblib.load("/Users/svyatoslavkalina/Desktop/Projects/Data_Science_ML/Model_deployment/final_model.pkl") 
+    col_names = joblib.load("/Users/svyatoslavkalina/Desktop/Projects/Data_Science_ML/Model_deployment/column_names.pkl") 
+
+    app.run(debug=True)
